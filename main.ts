5b4c70b8bd6b84ea8a12408228579b6b3029a82b
kayleigh362 <111659750+kayleigh362@users.noreply.github.com>
2022-08-20 20:36:04 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (randint(0, 10)))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showArrow(ArrowNames.NorthWest)
+})
+basic.showString("" + (Math.randomBoolean()))
+if (false) {
+	
+}
+if (true) {
+    basic.showString("Hello!")
+}
+basic.forever(function () {
+	
+})
